924cd4ce040340461030015514b620f8f2842546
LimJang <ogf2002@naver.com>
2024-01-24 19:27:30 +0900

    Initial Commit

---
+++ GithubTest/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GithubTest
+//
+//  Created by 임정환의 맥북 on 1/24/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
